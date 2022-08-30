e1290599858b01b63b6e6a2fabbdce9b529a1940
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-30 07:52:47 +0300

    lab completed

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelector('#modal').className='hidden'
+const likes=document.querySelectorAll('.like-glyph')
+function simpLikes(){
+  likes.forEach(function(press){
+    press.addEventListener('click', function(event){
+      event.preventDefault();
 
+      if (event.target.innerHTML==EMPTY_HEART){
+        mimicServerCall("")
+        .then(function(response){
+          event.target.innerHTML=FULL_HEART
+          event.target.classList.add('activated-heart')
 
+        })
 
+      }
+      else{
+        event.target.innerHTML=EMPTY_HEART
+        event.target.classList.remove('activated-heart')
+      }
+    })
+    .catch((error) =>{
+      const divError=document.querySelector('.hidden')
+      divError.classList.remove('hidden')
+      divError.textContent=error
+      setTimeout(()=> divError.classList.add('hidden'),3000)
+    })
+  })
+}
+
+simpLikes()
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
